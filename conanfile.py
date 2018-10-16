cb2c120e3b29a190eecae2f4d42c5a75633bbe55
Uilian Ries <uilianries@gmail.com>
2018-10-16 12:30:30 -0300

    Apply conan conventions
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- conanfile.py
+++ conanfile.py
@@ -14,7 +14,7 @@ class FollyConan(ConanFile):
     url = "https://github.com/bincrafters/conan-folly"
     homepage = "https://github.com/facebook/folly"
     author = "Bincrafters <bincrafters@gmail.com>"
-    license = "Apache 2.0"
+    license = "Apache-2.0"
     settings = "os", "arch", "compiler", "build_type"
     options = {"shared": [True, False], "fPIC": [True, False]}
     default_options = {"shared": False, "fPIC": True}
@@ -32,8 +32,7 @@ class FollyConan(ConanFile):
         "zlib/1.2.11@conan/stable",
         "zstd/1.3.5@bincrafters/stable",
     )
-    source_subfolder = "source_subfolder"
-    build_folder = "build_folder"
+    _source_subfolder = "source_subfolder"
 
     def config_options(self):
         if self.settings.os == "Windows":
@@ -48,20 +47,20 @@ class FollyConan(ConanFile):
     def source(self):
         tools.get("{0}/archive/v{1}.tar.gz".format(self.homepage, self.release))
         extracted_dir = self.name + "-" + self.release
-        os.rename(extracted_dir, self.source_subfolder)
+        os.rename(extracted_dir, self._source_subfolder)
         
     def _configure_cmake(self):
         cmake = CMake(self)
-        cmake.configure(build_folder=self.build_folder)
+        cmake.configure()
         return cmake
         
     def build(self):
-        tools.patch(base_path=self.source_subfolder, patch_file='folly.patch')
+        tools.patch(base_path=self._source_subfolder, patch_file='folly.patch')
         cmake = self._configure_cmake()
         cmake.build()
 
     def package(self):
-        self.copy(pattern="LICENSE.md", dst="licenses", src=self.source_subfolder)
+        self.copy(pattern="LICENSE.md", dst="licenses", src=self._source_subfolder)
         cmake = self._configure_cmake()
         cmake.install()
 
