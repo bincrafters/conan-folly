28e176241f2364e655416c382904a2de06fe935e
Uilian Ries <uilianries@gmail.com>
2018-11-18 22:20:56 -0200

    #350 Add URI on package test
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- test_package/test_package.cpp
+++ test_package/test_package.cpp
@@ -4,18 +4,20 @@
 #include <folly/Format.h>
 #include <folly/futures/Future.h>
 #include <folly/executors/ThreadedExecutor.h>
+#include <folly/Uri.h>
 
-static void print(const int value) {
-    const auto str = folly::format("The answer is {}", value);
-    std::cout << str << std::endl;
+static void print_uri(const std::string& value) {
+    const folly::Uri uri(value);
+    const auto authority = folly::format("The authority from {} is {}", value, uri.authority());
+    std::cout << authority << std::endl;
 }
 
 int main() {
     folly::ThreadedExecutor executor;
-    folly::Promise<int> promise;
-    folly::Future<int> future = promise.getSemiFuture().via(&executor);
-    folly::Future<folly::Unit> unit = std::move(future).thenValue(print);
-    promise.setValue(42);
+    folly::Promise<std::string> promise;
+    folly::Future<std::string> future = promise.getSemiFuture().via(&executor);
+    folly::Future<folly::Unit> unit = std::move(future).thenValue(print_uri);
+    promise.setValue("https://github.com/bincrafters");
     std::move(unit).get();
     return EXIT_SUCCESS;
 }
